b69475a0cbf0438f4c79482144aeac8c05ca645d
Vadim <snooperv@gmail.com>
2022-05-27 10:47:25 +0500

    Задание

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [lastMessage, setLastMessage] = useState(-1);
+    const callback = (message: number) => setLastMessage(message);
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setLastMessage(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {lastMessage}
+        </div>
+    );
 }
